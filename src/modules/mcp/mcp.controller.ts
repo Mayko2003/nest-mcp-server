1042d0d59b1dbc89efec1b58ceda5338a472f107
Mayko2003 <maykox34f@gmail.com>
2025-05-13 18:03:23 -0300

    Mejora el manejo de errores en MCPController al personalizar el mensaje de error en la respuesta JSON y refactoriza el cierre de conexiones para mayor claridad en el código.

--- src/modules/mcp/mcp.controller.ts
+++ src/modules/mcp/mcp.controller.ts
@@ -10,27 +10,35 @@ export class MCPController {
 
   @Post('/')
   async postController(@Req() req: Request, @Res() res: Response) {
+    console.log('New request');
+    console.log('MCP Request', req.body);
     try {
       const server = this.mcpService.getServer();
       const transport: StreamableHTTPServerTransport =
         new StreamableHTTPServerTransport({
           sessionIdGenerator: undefined,
         });
+      const closeConnection = async () => {
+        await transport.close();
+        await server.close();
+        this.logger.log('Server & transport closed');
+      };
       res.on('close', () => {
         this.logger.log('Request closed, closing server...');
-        transport.close();
-        server.close();
+        closeConnection();
       });
       await server.connect(transport);
       await transport.handleRequest(req, res, req.body);
     } catch (error) {
       this.logger.error(error);
+      const { message } = error as Error;
+      const errorMessage = message || 'Internal server error';
       if (!res.headersSent) {
         res.status(500).json({
           jsonrpc: '2.0',
           error: {
             code: -32603,
-            message: 'Internal server error',
+            message: errorMessage,
           },
           id: null,
         });
